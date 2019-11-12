9cdf4a08fe2ab8595230e557d12b9a17cc55b397
OrangeFlag <25643404+OrangeFlag@users.noreply.github.com>
2019-11-12 17:36:21 +0300

    feat: deploy

--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,3 @@
 use Mix.Config
+
+config :url_history_server, UrlHistory.Application, [redis_host: "localhost", redis_port: 6379]
--- config/prod.exs
+++ config/prod.exs
@@ -1,1 +1,5 @@
 use Mix.Config
+
+config :url_history_server, UrlHistory.Server.Endpoints, port: System.get_env("PORT")
+
+config :url_history_server, UrlHistory.Application, [redis_host: System.get_env("REDIS_URL"), redis_port: 6379]
