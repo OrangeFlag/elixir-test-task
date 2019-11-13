0c7aca81fb909fe4717d7060c4956d23e4a35b35
OrangeFlag <25643404+OrangeFlag@users.noreply.github.com>
2019-11-13 03:22:46 +0300

    fix: double definition of redis_host

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,5 @@
 use Mix.Config
 
-config :url_history_server, cowboy_port: 8080
-
 config :url_history_server, :redis, module: UrlHistory.Repository.Redis.Impl
 
 config :url_history_server, cowboy_port: (System.get_env("PORT") || "8080") |> String.to_integer()
